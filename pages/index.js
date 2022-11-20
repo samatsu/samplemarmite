3a4d982d96f74505d4a5a0e327ecbf81a4707247
samatsu <samatsu@outlook.jp>
2022-11-20 11:19:14 +0000

    first init

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import { createClient } from 'contentful'
+import RecipeCard from '../components/RecipeCard'
+
+export default function Recipes({ recipes }) {
+  return (
+    <div className="recipe-list">
+      {recipes.map(recipe => (
+          <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+      <style jsx>{`
+          .recipe-list{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
+    </div>
+  )
+}
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACESS_KEY,
+  })
+
+  const res = await client.getEntries(
+    {
+      content_type: 'recipe'
+    }
+  )
+
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+}
+
