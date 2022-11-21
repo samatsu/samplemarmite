f7dcc35be34c95e356be460d0560fba8b85e52f3
samatsu <samatsu@outlook.jp>
2022-11-21 02:02:27 +0000

    fallback

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -22,7 +22,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 export const getStaticProps = async ({ params }) => {
@@ -40,6 +40,7 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function RecipeDetails({ recipe }) {
+  if(!recipe) return <div>loading...</div>
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
   return (
     <div>
